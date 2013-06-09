bac9dd16067654666076c81e075cd647d3544b67
vijay-shanker <deontics@gmail.com>
2013-06-10 00:22:15 +0530

    added lyrics view

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from lyrics.views import BandList, AlbumList
+from lyrics.views import BandList, AlbumList, LyricsView
 
 urlpatterns = patterns('',
     url(r'bands-list/$', BandList.as_view(), name="bands-list"),
-    url(r'album-list/(?P<band_id>\d+)/$', AlbumList.as_view(), name="album-list")
+    url(r'album-list/(?P<band_id>\d+)/$', AlbumList.as_view(), name="album-list"),
+    url(r'show-lyrics/(?P<pk>\d+)/$',LyricsView.as_view(), name="lyrics-view"),
     )
--- lyrics/views.py
+++ lyrics/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from django.shortcuts import get_list_or_404
-from lyrics.models import Band, Album
+from django.shortcuts import get_list_or_404, get_object_or_404
+from lyrics.models import Band, Album, Song
 
 class BandList(ListView):
     model = Band
@@ -19,6 +19,17 @@ class AlbumList(DetailView):
     
     def get_object(self, queryset=None):
         return get_list_or_404(Album, band__id = self.kwargs['band_id'])
+
+class LyricsView(DetailView):
+    model = Song
+    template_name = 'lyrics/lyrics.html'
+    context_object_name = 'lyrics'
+    
+    def get_object_name(self):
+        return get_object_or_404(Song, id = self.kwargs['pk'])
+        
+    
+
     
     
     
