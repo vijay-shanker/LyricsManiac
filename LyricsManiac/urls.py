f3c765fd56f1e6fb5a5b41715ded4001de4f056c
vijay-shanker <deontics@gmail.com>
2013-06-11 23:10:54 +0530

    introduced to bootstrap

--- LyricsManiac/urls.py
+++ LyricsManiac/urls.py
@@ -1,3 +1,4 @@
+import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -16,3 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
